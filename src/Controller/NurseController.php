ed3a9b41ca83db63b0f0f2b5ad2daaac60226d49
Woocky17 <davidortetsabater@gmail.com>
2024-10-01 16:44:06 +0200

    Implementation of the login

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -4,16 +4,46 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class NurseController extends AbstractController
 {
-    #[Route('/login', name: 'app_nurse')]
-    public function login(): JsonResponse
+    #[Route('/login', name: 'app_nurse', methods:['POST'])]
+    public function login(Request $request): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/NurseController.php',
-        ]);
+        $gmail = $request->request->get('gmail');
+        $password = $request->request->get('password');
+
+        if (is_null($gmail) || is_null($password)) {
+            return $this->json(['error' => 'Missing parameters'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = [
+            [
+                'id' => 1,
+                'nombre' => 'David',
+                'gmail' => 'david@example.com',
+                'password' => '1234'
+            ],
+            [
+                'id' => 2,
+                'nombre' => 'Maria',
+                'gmail' => 'maria@example.com',
+                'password' => 'abcd'
+            ]
+        ];
+
+        
+
+        foreach ($data as $user) {
+
+            if ($user['gmail'] === $gmail && $user['password'] === $password) {
+                return $this->json(true) ;
+            }
+        }
+        return $this->json(false);
+        
     }
 }
